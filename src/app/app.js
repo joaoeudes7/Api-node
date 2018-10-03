0fd0c4c07d4da6babd85c920cc44348b427bae49
João Eudes Lima <joaoeudes7@gmail.com>
2018-10-02 23:16:47 -0300

    Add Global error handler and

--- src/app/app.js
+++ src/app/app.js
@@ -2,10 +2,12 @@ import express from 'express';
 
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
+import errorHandler from './middlewares/errorHandler';
 
 import Projects from './routes/Project.routes';
 import Users from './routes/Users.routes';
 import Auth from './routes/Auth.routes';
+import Register from './routes/Register.routes';
 
 const app = express();
 const port = 3000;
@@ -18,8 +20,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(helmet());
 
 app.use('/auth', Auth);
+app.use('/register', Register);
 app.use('/projects', Projects);
 app.use('/user', Users);
+
+// global error handler
+app.use(errorHandler)
 
 app.get('/', (req, res) => {
   res.send(`The API-REST is Online`);
