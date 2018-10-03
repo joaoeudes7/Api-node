cf63112bcd4e2e99f0acb96aba20cb6682b85ff6
João Eudes Lima <joaoeudes7@gmail.com>
2018-10-02 23:16:56 -0300

    fix bugs

--- src/app/config/database.js
+++ src/app/config/database.js
@@ -1,6 +1,10 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://localhost:27017/api', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/api',
+{
+  useCreateIndex: true,
+  useNewUrlParser: true
+});
 mongoose.Promise = Promise; // Mongoose using ES6
 
 export default mongoose;
