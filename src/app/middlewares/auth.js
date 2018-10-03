3b45e1a5349d997dfcbb7f4d1f37e57f2d51d6c4
João Eudes Lima <joaoeudes7@gmail.com>
2018-10-02 23:14:32 -0300

    Set authorization as Bearer

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -2,11 +2,14 @@ import jwt from 'jsonwebtoken';
 import authConfig from '../config/auth';
 
 export default (req, res, next) => {
-  const token = req.headers.authorization;
+  const authorization = req.headers.authorization;
+  const regex = new RegExp(/^Bearer\s[\w|\W]{10,}$/);
 
-  if (!token) {
+  if (!authorization || !regex.test(authorization)) {
     return res.status(401).send({ error: "No token provided" });
   }
+
+  const token = authorization.split(' ')[1];
 
   jwt.verify(token, authConfig.secret, (err, decoded) => {
     if (err) {
